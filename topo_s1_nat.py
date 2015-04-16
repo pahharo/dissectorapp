558415092dab545758d51d520c12019b6a211fdf
Manuel Sánchez López <manusl.teleco@gmail.com>
2015-04-16 13:45:16 +0200

    modificacion de archivos .java con licencia 2

--- topo_s1_nat.py
+++ topo_s1_nat.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
 
+"""
+Copyright (C) 2015 Manuel Sánchez López
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have getTransmitErrorCountd a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from mininet.net import Mininet
 from mininet.node import Controller, RemoteController, OVSController
 from mininet.node import CPULimitedHost, Host, Node
